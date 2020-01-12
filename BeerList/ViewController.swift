4e0dd591c353f9672a6bddb56db81b9e000abda2
e-sung <dev.esung@gmail.com>
2020-01-12 17:21:17 +0900

    Initial Commit

---
+++ BeerList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerList
+//
+//  Created by 류성두 on 2020/01/12.
+//  Copyright © 2020 Sungdoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
